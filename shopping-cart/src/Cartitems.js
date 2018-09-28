01b45d274f1385b60bccdcf75f753ba7ad99ee7b
Kerry Abernathy <Kerrysage89@gmail.com>
2018-09-28 10:58:52 -0600

    Added Header, Footer, and Cart Items

---
+++ shopping-cart/src/Cartitems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cartitems extends React.Component {
+    render() {
+        return (
+            <div className="container">
+            <h1>Cart Items</h1>
+            </div>
+        )
+}
+}
+
+export default Cartitems
---
+++ shopping-cart/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CartFooter extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">&copy; 2018</a>
+            </nav>
+        )
+}
+}
+
+export default CartFooter
