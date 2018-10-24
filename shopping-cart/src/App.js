9ffab5bcb57d90104a33289e2e944e402fe49344
Kerry Abernathy <Kerrysage89@gmail.com>
2018-10-24 14:31:17 -0600

    added counter, set values right

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer'
 import CartItems from './CartItems'
+import CartItem from './CartItem'
 import AddItems from './AddItems'
 import './App.css';
 
@@ -26,14 +27,47 @@ class App extends Component {
       { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
       { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
       { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
-]
-  }}
+], 
+    currentName: null,
+    currentPride: null,
+    currentQuantity: null,
+    currentTotal: 0
+  }
+}
+  modProduct = (e) => {
+    e.preventDefault()
+    if(product.name === e.target.value) {
+      this.setState({currentName: product.name})
+      this.setState({currentPrice: product.priceInCents})
+      this.setState({currentId: product.id})
+    }
+  }
+
+    modQuantity = (e) => {
+      e.preventDefault()
+      this.setState({currentQuantity: e.target.value})
+    }
+
+    total = (arr) => {
+      var counter = 0
+      arr.map(item => {
+        counter += ((item.priceInCents * item.quantity) / 100)
+      })
+      return counter
+    }
+
+    modify = () => {
+      const newcart = this.state.cartItemsList.concat({id: this.state.cartItemsList.length+1, product: {id: this.state.currentId, name: this.state.currentName, priceInCents: this.state.currentPrice}, quantity: this.state.currentQuantity})
+        this.setState({cartItemsList: newcart}, () => (this.state))
+        var counter = 0
+    }
+
   render() {
     return (
       <div >
         <Header />
-        <CartItems cartItemsList={this.state.cartItemsList}/>
-        <AddItems AddItemList={this.state.AddItemList}/> 
+        <CartItems cartItemsList={this.state.cartItemsList} total={this.total}/>>
+        <AddItems AddItemList={this.state.AddItemList} modify={this.modify} modQuantity={this.modQuantity} modProduct={this.modProduct}/> 
         <Footer list="2018"/>
       </div>
     )
